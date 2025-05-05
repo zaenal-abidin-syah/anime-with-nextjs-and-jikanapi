69b1e6150bd2855f2a0d63bf2e9f06baece51789
zaenal-abidin-syah <aszaenal618@gmail.com>
2025-05-05 22:58:45 +0700

    fix deploy

--- components/Layouts/SearchManga.tsx
+++ components/Layouts/SearchManga.tsx
@@ -1,40 +1,39 @@
-import React from 'react'
-import Card from '../Fragments/Card'
+import React from "react";
+import Card from "../Fragments/Card";
 // import axios from 'axios'
-import getRecomendation from '@/services/getRecomendation'
-import {TopTypes} from '@/types/toptypes'
-import getTop from '@/services/getTop'
+import getRecomendation from "@/services/getRecomendation";
+import { TopTypes } from "@/types/toptypes";
+import getTop from "@/services/getTop";
 
-
-async function SearchManga({
-  result,
-}:{result:TopTypes}) {
-  console.log(result)
+interface SearchMangaProps {
+  result?: TopTypes[]; // ← note the [] here
+}
+async function SearchManga({ result }: SearchMangaProps) {
+  console.log(result);
   // let query :string = searchParams.query
-  
+
   // const result = await search('manga', query ?? false)
 
   return (
-    <div className='container flex flex-col mx-auto py-10 gap-y-10'>
-      <div className='text-tersier font-bold text-4xl border-b-4 border-stone-200 border-double'>
+    <div className="container flex flex-col mx-auto py-10 gap-y-10">
+      <div className="text-tersier font-bold text-4xl border-b-4 border-stone-200 border-double">
         <h1>Search Manga</h1>
       </div>
-      <div className='flex flex-wrap gap-x-2 gap-y-8 justify-between'>
-      {result.map((data)=>{
+      <div className="flex flex-wrap gap-x-2 gap-y-8 justify-between">
+        {result?.map((data) => {
           return (
-            <Card 
+            <Card
               key={data.mal_id}
-              title={data.title} 
-              img={data.images.webp.image_url} 
+              title={data.title}
+              img={data.images.webp.image_url}
               alt={data.images.jpg.image_url}
               id={data.mal_id}
             />
-          )
+          );
         })}
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default SearchManga
+export default SearchManga;
